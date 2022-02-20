a95d49bb80f7e6b7842b264a8b5415ef7ce5137a
k3ith99 <hamza_saleh1999@hotmail.com>
2022-02-20 18:07:56 +0000

    created class for models

--- server/db/reviews.js
+++ server/db/reviews.js
@@ -4,8 +4,10 @@ db.reviews.insertMany([
     {name: "AC Origins", year: 2017, rating: 8},
     {name: "AC Odyssey", year:2018, rating: 9}, 
     {name: "Star wars Jedi Fallen Order", year: 2019, rating: 7.5},
-    {name: "DBZ Kakarot", year:2020, rating: 8/10},
-    {name: "god of war", year:2018, rating:9/10}
+    {name: "DBZ Kakarot", year:2020, rating: 8},
+    {name: "god of war", year:2018, rating:9}
 
 
 ])
+
+
